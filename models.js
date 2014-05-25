edb4eabaf538b49a7ecba3d39406dabffc6ce039
Daniel <dxu429@gmail.com>
2014-05-25 12:39:12 -0700

    merging

--- models.js
+++ models.js
@@ -1,12 +1,31 @@
-
 var Mongoose = require('mongoose');
 
+var CourseSchema = new Mongoose.Schema({
+  "name": String, 
+  "professor": String,
+  "num_students": Number,
 
-var ImgSchema = new Mongoose.Schema({
-	"image" : { type: String },
-	"hashtag" : { type: String}
 });
 
-exports.Img = Mongoose.model('Img', ImgSchema);
+var StudentSchema = new Mongoose.Schema({
+  // fields are defined here
+  "name": String, 
+  "student_id": String,
+  "email": String,
+  "phone_no": String,
+  "group_id": Mongoose.Schema.Types.ObjectId
+});
 
+var GroupSchema = new Mongoose.Schema({
+  "id": String,
+  "name": String
+});
 
+var UserGroupsSchema = new Mongoose.Schema({
+  "user_id": String,
+  "group_id": String
+});
+
+exports.User = Mongoose.model('User', UserSchema);
+exports.Group = Mongoose.model('Group', GroupSchema);
+exports.UserGroups = Mongoose.model('UserGroups', UserGroupsSchema);
