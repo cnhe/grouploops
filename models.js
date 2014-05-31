9efebc0f6b1c31ebd56a851c842d0b673ac93595
Daniel <dxu429@gmail.com>
2014-05-31 02:17:22 -0700

    updated models

--- models.js
+++ models.js
@@ -1,28 +1,46 @@
-var Mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-var CourseSchema = new Mongoose.Schema({
+var CourseSchema = new mongoose.Schema({
   "name": String, 
   "professor": String,
-  "num_students": Number,
-
+  "num_students": Number
 });
 
-var StudentSchema = new Mongoose.Schema({
+var CustomQuestionSchema = new mongoose.Schema({
+  "course_id": mongoose.Schema.Types.ObjectId,
+  "label": String,
+  "type": String
+});
+
+var StudentSchema = new mongoose.Schema({
   // fields are defined here
   "name": String, 
   "student_id": String,
   "email": String,
-  "phone_no": String,
-  "group_id": Mongoose.Schema.Types.ObjectId
+  "phone_no": String
+//  "course_id": mongoose.Schema.Types.ObjectId   // only if non distinct students
 });
 
-var GroupSchema = new Mongoose.Schema({
-  "id": String,
+var GroupSchema = new mongoose.Schema({
   "name": String
 });
 
-var UserGroupsSchema = new Mongoose.Schema({
-  "user_id": String,
-  "group_id": String
+// This is needed if we have "accounts" (i.e. logins)
+//   If we have accounts, there will be unique students and they each 
+//    have multiple courses
+var StudentCourseSchema = new mongoose.Schema({
+  "student_id": mongoose.Schema.Types.ObjectId, 
+  "course_id": mongoose.Schema.Types.ObjectId
 });
 
+var StudentGroupsSchema = new mongoose.Schema({
+  "student_id": mongoose.Schema.Types.ObjectId,
+  "group_id": mongoose.Schema.Types.ObjectId 
+});
+
+var Course = mongoose.model('Course', CourseSchema);
+var Student = mongoose.model('Student', StudentSchema);
+var Group = mongoose.model('Group', GroupSchema);
+var StudentCourse = mongoose.model('StudentCourse', StudentCourseSchema);
+var StudentGroups = mongoose.model('StudentGroups', StudentGroupsSchema);
+var CustomQuestion = mongoose.model('CustomQuestion', CustomQuestionSchema);
